ec713615b24f129267847776fc834167da44b6d1
islambekk23 <islambekkunabaev@gmail.com>
2024-08-07 14:33:33 +0300

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,15 @@
+my_dict = {'Islambek': 1998}
+print(my_dict)
+my_dict = {'Islambek': 1998, 'Aman': 1989, 'Rasulbek': 2000}
+print(my_dict)
+print(my_dict['Rasulbek'])
+my_dict.update({'Alibek': 1999, 'Navoiy': 1441})
+print(my_dict)
+print(my_dict.get('Qanat'))
+del my_dict['Rasulbek']
+print(my_dict)
+
+my_set_ = {'Manchester', 55, 5.75, (23,17), 'Manchester', 55, 5.75}
+print(my_set_)
+print(my_set_.update({77, (3.14,888)}))
+print(my_set_)
